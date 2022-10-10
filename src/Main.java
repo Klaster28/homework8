ec4b95328a2ab055a7316856171096864390fb9c
Klaster28 <89096914521@mail.ru>
2022-10-10 21:57:05 +0300

    Коммит после задания 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+    // Задача 1.1.1
+        int [] numberBoxes = new int[3];
+        numberBoxes [0] = 1;
+        numberBoxes [1] = 2;
+        numberBoxes [2] = 3;
+        System.out.println(numberBoxes [0]);
+        System.out.println(numberBoxes [1]);
+        System.out.println(numberBoxes [2]);
+
+    // Задача 1.1.2
+        double [] exactWeightGrams = {1.57, 7.654, 9.986};
+        System.out.println (exactWeightGrams [0]);
+        System.out.println (exactWeightGrams [1]);
+        System.out.println (exactWeightGrams [2]);
+
+    // Задача 1.1.3
+        int [] penColorBob = {1,2,3,3,2,4,5,6};
+        System.out.println(penColorBob [0]);
+
+    // Задача 1.2
+
+
 
 
     }
+
+
+
 }
